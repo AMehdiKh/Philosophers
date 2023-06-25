0b872f29cd0dfcaa236ec8be5801dfcb2798861d
Ahmed El Mehdi El Khelloufi <ael-khel@c1r3p10.1337.ma>
2023-06-25 11:12:46 +0100

    almost finished or atleast that what I think

--- philo.c
+++ philo.c
@@ -6,16 +6,108 @@
 /*   By: ael-khel <ael-khel@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 19:25:46 by ael-khel          #+#    #+#             */
-/*   Updated: 2023/06/19 21:46:55 by ael-khel         ###   ########.fr       */
+/*   Updated: 2023/06/25 10:53:28 by ael-khel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <pthread.h>
+#include <stdlib.h>
+
+int	ft_eating(t_switch *context)
+{
+	pthread_mutex_lock(&context->eating);
+	if (context->death)
+		return (pthread_mutex_unlock(&context->eating), EXIT_FAILURE);
+	context->philo->last_meal = ft_time_ms(&context->philo->time);
+	printf("%d %d has taken a fork\n", context->philo->last_meal);
+	printf("%d %d is eating\n", ft_time_ms(&context->philo->time));
+	while (ft_time_ms(&context->philo->time)
+		< (context->philo->last_meal + context->t_eat) && !is_dead(context))
+		;
+	if (context->death)
+		return (pthread_mutex_unlock(&context->eating), EXIT_FAILURE);
+	return (pthread_mutex_unlock(&context->eating), EXIT_SUCCESS);
+}
+
+int	ft_sleeping(t_switch *context)
+{
+	pthread_mutex_lock(&context->sleeping);
+	if (context->death)
+		return (pthread_mutex_unlock(&context->sleeping), EXIT_FAILURE);
+	printf("%d %d is sleeping\n", ft_time_ms(&context->philo->time));
+	while (ft_time_ms(&context->philo->time)
+		< (context->philo->last_meal + context->t_eat + context->t_sleep)
+		&& !is_dead(context))
+		;
+	if (context->death)
+		return (pthread_mutex_unlock(&context->sleeping), EXIT_FAILURE);
+	printf("%d %d is thinking\n", ft_time_ms(&context->philo->time));
+	return (pthread_mutex_unlock(&context->sleeping), EXIT_SUCCESS);
+}
+
+int	is_dead(t_switch *context)
+{
+	size_t	death_time;
+
+	death_time = ft_time_ms(&context->philo->time);
+	if (death_time >= (context->philo->last_meal + context->t_die))
+	{
+		pthread_mutex_lock(&context->died);
+		if (!context->death)
+		{
+			context->death = 1;
+			printf("%d %d died\n", death_time);
+		}
+		return (pthread_mutex_unlock(&context->died), EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+void	*routine(void *arg)
+{
+	t_switch	*context;
+	size_t		id;		
+
+	context = arg;
+	id = context->philoid;
+	while (!context->death)
+	{
+		if (ft_eating(context))
+			return (NULL);
+		if (ft_sleeping(context))
+			return (NULL);
+	}
+	return (NULL);
+}
 
 int	main(int ac, char **av)
 {
-	if (ft_check_args(ac, av))
+	t_switch	*context;
+	int			i;
+
+	context = malloc(sizeof(t_switch));
+	if (!context)
 		return (EXIT_FAILURE);
-	
-	return (EXIT_SUCCESS);
+	if (ft_check_args(memset(context, 0, sizeof(t_switch)), ac, av))
+		return (free(context), EXIT_FAILURE);
+	context->philo = malloc(sizeof(t_philo) * context->n_philo);
+	if (!context->philo)
+		return (free(context), EXIT_FAILURE);
+	pthread_mutex_init(&context->eating, NULL);
+	pthread_mutex_init(&context->sleeping, NULL);
+	pthread_mutex_init(&context->died, NULL);
+	i = -1;
+	while (++i < context->n_philo)
+	{
+		
+		pthread_create(&context->philo[i].thread, NULL, routine, context);
+	}
+	i = -1;
+	while (++i < context->n_philo)
+		pthread_join(context->philo[i].thread, NULL);
+	pthread_mutex_destroy(&context->eating);
+	pthread_mutex_destroy(&context->sleeping);
+	pthread_mutex_destroy(&context->died);
+	return (free(context->philo), free(context), EXIT_SUCCESS);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: ael-khel <ael-khel@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 18:58:33 by ael-khel          #+#    #+#             */
-/*   Updated: 2023/06/19 21:52:20 by ael-khel         ###   ########.fr       */
+/*   Updated: 2023/06/25 10:58:00 by ael-khel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,41 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <sys/time.h>
+# include <string.h>
 
 typedef struct s_philo
 {
-	pthread_t		thread_id;
-	pthread_mutex_t	fork;
+	struct timeval	time;
+	pthread_t		thread;
+	size_t			last_meal;
+	size_t			id;
 }	t_philo;
 
-typedef struct s_switching
+typedef struct s_switch
 {
-	int		n_philo;
-	int		t_die;
-	int		t_eat;
-	int		t_sleep;
-	int		n_times;
-	t_philo	*philo;
-}	t_switching;
+	int				n_philo;
+	int				t_die;
+	int				t_eat;
+	int				t_sleep;
+	int				n_times;
+	int				death;
+	int				id;
+	pthread_t		*thread;
+	t_philo			*philo;
+	pthread_mutex_t	eating;
+	pthread_mutex_t	sleeping;
+	pthread_mutex_t	died;
+}	t_switch;
+
+void	*routine(void *arg);
+int		is_dead(t_switch *context);
+int		ft_sleeping(t_switch *context);
+int		ft_eating(t_switch *context);
+size_t	ft_strlen(const char *s);
+long	ft_atol(const char *s);
+int		error_args(int ac);
+int		ft_check_args(t_switch *context, int ac, char **av);
+size_t	ft_time_ms(struct timeval *time);
 
 #endif
